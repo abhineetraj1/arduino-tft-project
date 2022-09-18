3aee68912a41c627e3bc7b42b93ac744f9a21c7b
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2022-09-18 06:50:08 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+#Arduino tft display project
+
+## Requirements
+* Arduino UNO board
+* Arduino tft display
+
+
+# Installation
+* Download Arduino IDE.
+* Open main.ino file and upload the code into arduino board.
+* Attach tft display to arduino board
+* Run your Arduino :)
+
+# Programming languages used:-
+<a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width="40" height="40"/> </a>
---
+++ main.ino
@@ -0,0 +1,85 @@
+#include <Adafruit_TFTLCD.h> 
+#include <Adafruit_GFX.h>    
+#include <TouchScreen.h>
+
+#define LCD_CS A3 
+#define LCD_CD A2 
+#define LCD_WR A1 
+#define LCD_RD A0 
+#define LCD_RESET A4 
+
+#define TS_MINX 122
+#define TS_MINY 111
+#define TS_MAXX 942
+#define TS_MAXY 890
+
+#define YP A3
+#define XM A2
+#define YM 9
+#define XP 8
+
+#define BLACK   0x0000
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 364);
+
+boolean buttonEnabled = true;
+
+void setup() {
+  
+  tft.reset();
+  uint16_t identifier = tft.readID();
+  tft.begin(identifier);
+  tft.setRotation(1);
+  tft.fillScreen(WHITE);
+  tft.drawRect(0,0,319,240,YELLOW);
+  
+  tft.setCursor(30,40);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(2);
+  tft.print("TFT LCD Touch Screen");
+  
+  tft.setCursor(115,80);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(2);
+  tft.print("Project\n\n            by");
+  
+  tft.setCursor(30,150);
+  tft.setTextColor(BLUE);
+  tft.setTextSize(2);
+  tft.print("Abhineet Raj");
+  
+  tft.fillRect(50,180, 210, 40, BLACK);
+  tft.drawRect(50,180,210,40,BLACK);
+  tft.setCursor(60,190);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(2);
+  tft.print("Follow github");
+
+}
+
+void loop() {
+  TSPoint p = ts.getPoint();
+  
+  if (p.z > ts.pressureThreshhold) {
+    
+   p.x = map(p.x, TS_MAXX, TS_MINX, 0, 320);
+   p.y = map(p.y, TS_MAXY, TS_MINY, 0, 480);
+       
+   if(p.x>50 && p.x<260 && p.y>180 && p.y<270 && buttonEnabled){
+    
+    buttonEnabled = false;
+    
+    pinMode(XM, OUTPUT);
+    pinMode(YP, OUTPUT);
+    
+    tft.fillScreen(WHITE);
+    tft.setCursor(50,70);
+    tft.setTextColor(BLACK);
+    tft.setTextSize(3);
+    tft.print(" Thank you\n\n -> Star your favourite\n repository in my \n github account");
+   }  
+  }
+}
