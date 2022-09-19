138f71cfde2b1ffe532eaedd1e5dc0d1e7908163
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2022-09-19 19:21:58 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -19,6 +19,12 @@
 #define XP 8
 
 #define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
